bcd54ae5ed6e784303ce10372c3030ea57c4b539
Saikat-C <97686210+Saikat-C@users.noreply.github.com>
2022-01-27 20:43:20 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,28 +1,91 @@
-var box;
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+var cloud,cloudImage
+
+
+
+var score;
+
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex2.png","trex3.png");
+  trex_collided = loadImage("trex_collided.png");
+  cloudImage=loadImage("cloud.png");
+  groundImage = loadImage("ground2.png");
+  
+ 
+  
+}
+
 function setup() {
-  createCanvas(400,400);
-box = createSprite(200,200,20,20);
+
+  createCanvas(600,200)
+  
+  //create a trex sprite
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
+  
+  //create a ground sprite
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -4;
+  
+  //creating invisible ground
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  //generate random numbers
+  var rand =  Math.round(random(1,100))
+  console.log(rand)
 
 }
 
-function draw() 
-{
-  background("gold");
-  if(keyIsDown (RIGHT_ARROW)){
-     box.position.x=box.position.x+5
-    }
-    if(keyIsDown (LEFT_ARROW)){
-      box.position.x=box.position.x-5
-     }
-     if(keyIsDown (UP_ARROW)){
-      box.position.y=box.position.y-5
-     }
-     if(keyIsDown (DOWN_ARROW)){
-      box.position.y=box.position.y+5
-     }
-    drawSprites();
+function draw() {
+  //set background color
+  background(180);
+
+  console.log(frameCount);
+  
+  
+  // jump when the space key is pressed
+  if(keyDown("space")&& trex.y >= 100) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  //stop trex from falling down
+  trex.collide(invisibleGround);
+  
+  //Spawn Clouds
+  spawnClouds();
+  
+  drawSprites();
 }
 
+//function to spawn the clouds
+function spawnClouds(){
+ // write your code here 
+ if(frameCount%60===0){
+
+ 
+ cloud=createSprite(700,50,20,20);
+cloud.addImage(cloudImage);
+cloud.y=Math.round(random(10,80));
+cloud.scale= 0.6;
+ cloud.velocityX= -5;
+console.log(trex.depth);
+console.log(cloud.depth);
+cloud.depth=trex.depth;
+trex.depth=trex.depth+1;
+
+}}
 
 
 
